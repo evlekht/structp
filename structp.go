5ed92a6fb4fae075041d183ef4778bce0571760b
Evgeny <evgeny.leht@cashelec.ru>
2020-09-30 15:55:42 +0300

    Custom line-ending.

--- structp.go
+++ structp.go
@@ -15,7 +15,19 @@ func getIndent(indent int) string {
 	return output
 }
 
-func print(v interface{}, diveIntoPointers bool, indent int, omitType bool) (output string) {
+func printField(field reflect.Value, fieldTypeOriginal reflect.Type, fieldName string, diveIntoPointers bool, indent int, lineEnding string) string {
+	switch field.Type().Kind() {
+	case reflect.Struct:
+		nestedOutput := print(field.Interface(), diveIntoPointers, indent, true, lineEnding)
+		return fmt.Sprintf("%s%s %v : %s,%s", getIndent(indent), fieldName, fieldTypeOriginal, nestedOutput, lineEnding)
+	case reflect.Ptr:
+		return fmt.Sprintf("%s%s %v : %v,%s", getIndent(indent), fieldName, fieldTypeOriginal, field.Pointer(), lineEnding)
+	default:
+		return fmt.Sprintf("%s%s %v : %v,%s", getIndent(indent), fieldName, fieldTypeOriginal, field.Interface(), lineEnding)
+	}
+}
+
+func print(v interface{}, diveIntoPointers bool, indent int, omitType bool, lineEnding string) (output string) {
 	defer func() {
 		if recover() != nil {
 			output += " <PANIC> "
@@ -25,47 +37,35 @@ func print(v interface{}, diveIntoPointers bool, indent int, omitType bool) (out
 	valueOfV := reflect.Indirect(reflect.ValueOf(v))
 	typeOfV := valueOfV.Type()
 
+	indentStr := getIndent(indent)
+
 	if typeOfV.Kind() != reflect.Struct {
 		return
 	}
 
-	indentStr := getIndent(indent)
-	innerIndentStr := indentStr + indentStrConst
-
 	if omitType {
-		output = fmt.Sprintf("{\n")
+		output = fmt.Sprintf("{%s", lineEnding)
 	} else {
-		output = fmt.Sprintf("%s%v {\n", indentStr, typeOfV)
+		output = fmt.Sprintf("%s%v {%s", indentStr, typeOfV, lineEnding)
 	}
 
 	for i := 0; i < typeOfV.NumField(); i++ {
 		field := valueOfV.Field(i)
 		fieldType := field.Type()
-	L:
-		for {
-			fieldTypeIndirect := field.Type()
-			switch fieldTypeIndirect.Kind() {
-			case reflect.Struct:
-				nestedOutput := print(field.Interface(), diveIntoPointers, indent+1, true)
-				output += fmt.Sprintf("%s%s %s : %s,\n", innerIndentStr, typeOfV.Field(i).Name, fieldType, nestedOutput)
-				break L
-			case reflect.Ptr:
-				if diveIntoPointers {
-					field = reflect.Indirect(field)
-				} else {
-					output += fmt.Sprintf("%s%s %s : %v,\n", innerIndentStr, typeOfV.Field(i).Name, fieldType, field.Pointer())
-					break L
-				}
-			default:
-				output += fmt.Sprintf("%s%s %s : %v,\n", innerIndentStr, typeOfV.Field(i).Name, fieldType, field.Interface())
-				break L
-			}
+		if diveIntoPointers && fieldType.Kind() == reflect.Ptr {
+			field = reflect.Indirect(field)
 		}
+		output += printField(field, fieldType, typeOfV.Field(i).Name, diveIntoPointers, indent+1, lineEnding)
 	}
 	output += fmt.Sprintf("%s}", indentStr)
 	return
 }
 
+// Print will separate lines with \n
 func Print(v interface{}, diveIntoPointers bool) string {
-	return print(v, diveIntoPointers, 0, false)
+	return print(v, diveIntoPointers, 0, false, "\n")
+}
+
+func PrintWithCustomLineEnding(v interface{}, diveIntoPointers bool, lineEnding string) string {
+	return print(v, diveIntoPointers, 0, false, lineEnding)
 }
